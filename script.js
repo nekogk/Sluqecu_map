3b3dc4759c8a93351add241e50ef578b5a68805f
daco <78739941+nekogk@users.noreply.github.com>
2026-09-05 00:44:41 +0900

    ssalmeok

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const bounds = [[0, 0], [mapSize, mapSize]];
 let currentLang = 'lo';
 let locationData = [];
 
-const zoomThresholds = {'b': -4, 'd': -2, 'e': 0};
-const zoomThresholdsDisappear = {'b': 0, 'd': 2, 'e': 4};
-const fontSizeThresholds = {'b': '4vh', 'd': '2vh', 'e': '2vh'};
-const ICON_RANKS = ['d', 'e'];
+const zoomThresholds = {'b': -4, 'c': -2, 'd': -1, 'e': 0};
+const zoomThresholdsDisappear = {'b': 0, 'c': 2, 'd': 3, 'e': 4};
+const fontSizeThresholds = {'b': '4vh', 'c': '2vh', 'd': '2vh', 'e': '2vh'};
+const ICON_RANKS = ['c', 'd', 'e'];
 const ICON_SCALE = 1.5;
 
 function vhToPx(vhString) {
